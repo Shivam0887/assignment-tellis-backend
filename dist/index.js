f77787304b68f3eb8ebaaddd9ff5ca4a3abd180b
shivam <shivamsharma0887@gmail.com>
2025-05-20 23:29:47 +0530

    update2: CORS_ORIGIN

--- dist/index.js
+++ dist/index.js
@@ -3,19 +3,20 @@ import cookieParser from "cookie-parser";
 import "dotenv/config";
 import cors from "cors";
 import router from "./routes/index.js";
+import { errorHandler } from "./middlewares/error-handler.middleware.js";
 import { ConnectToDB } from "./configs/database.js";
 const app = express();
 ConnectToDB();
 app.set("trust proxy", 1);
 app.use(cookieParser());
 app.use(cors({
-    origin: "https://zxsmwnmb-5173.inc1.devtunnels.ms",
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api", router);
-// app.use(errorHandler);
+app.use(errorHandler);
 app.listen(5000, () => {
     console.log("Server started...");
 });
