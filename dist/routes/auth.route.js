31627b9c84cd84c63bbf52ad34bc519c02628449
shivam <shivamsharma0887@gmail.com>
2025-05-22 03:16:16 +0530

    dist removed

--- dist/routes/auth.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { register, login, logout } from "../controllers/auth.controller.js";
-import { validateLogin, validateRegister, } from "../middlewares/validation.middleware.js";
-const router = express.Router();
-router.post("/register", validateRegister, register);
-router.post("/login", validateLogin, login);
-router.get("/logout", logout);
-export default router;
-//# sourceMappingURL=auth.route.js.map
